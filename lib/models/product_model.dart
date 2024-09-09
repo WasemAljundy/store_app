212719d17d5bd5d31e52982742bdc39590193dc1
WasemAljundy <wasemaljundy22@gmail.com>
2024-09-09 16:18:29 +0300

    Initial commit

---
+++ lib/models/product_model.dart
@@ -0,0 +1,48 @@
+class ProductModel {
+  final int id;
+  final String title;
+  final String description;
+  final String image;
+  final double price;
+  final int quantity;
+  final RatingModel rating;
+
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.image,
+    required this.price,
+    required this.quantity,
+    required this.rating,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      image: json['image'],
+      price: json['price'],
+      quantity: json['quantity'],
+      rating: RatingModel.fromJson(json['rating']),
+    );
+  }
+}
+
+class RatingModel {
+  final double rate;
+  final int count;
+
+  RatingModel({
+    required this.rate,
+    required this.count,
+  });
+
+  factory RatingModel.fromJson(Map<String, dynamic> json) {
+    return RatingModel(
+      rate: json['rate'],
+      count: json['count'],
+    );
+  }
+}
